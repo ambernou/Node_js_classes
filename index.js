62189323bc0fa40fceb9f383f37c8955fa7261d4
NatalyaSemenova <ambernou@gmail.com>
2021-12-10 00:14:35 +0700

    some bugs fixed

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 let colors = require('colors');
 
-let a = 8;
-let b = '10';
+const [x, y] = process.argv.slice(2);
+
+let a = +x;
+let b = +y;
 let arr = [];
 
-if (typeof a !== "number" || typeof b !== "number") {
+if (typeof a !== "number" || typeof b !== "number" || isNaN(a) || isNaN(b)) {
   console.log('Not a number!');
 } else {
   nextPrime:
@@ -15,30 +17,21 @@ if (typeof a !== "number" || typeof b !== "number") {
     }
     arr.push(i);
   }
-}
-
-if (arr.length == 0) {
-  console.log(colors.red('No prime numbers'));
-}
-
-let pos = 1;
-arr.forEach((x) => {
-  if (pos == 1) {
-    console.log(colors.green(x));
-    pos++;
-  } else if (pos == 2) {
-    console.log(colors.yellow(x));
-    pos++;
-  } else if (pos == 3) {
-    console.log(colors.red(x));
-    pos = 1;
+  if (arr.length == 0) {
+    console.log(colors.red('No prime numbers'));
   }
-});
-
-// for (i = 0; i < arr.length; i = i + 3) {
-//   console.log(arr[i], '-1-');
-//   if (i != arr.length) {
-//   console.log(arr[i + 1], '-2-');
-//   console.log(arr[i + 2], '-3-');
-//   } else break;
-// }
+  
+  let pos = 1;
+  arr.forEach((x) => {
+    if (pos == 1) {
+      console.log(colors.green(x));
+      pos++;
+    } else if (pos == 2) {
+      console.log(colors.yellow(x));
+      pos++;
+    } else if (pos == 3) {
+      console.log(colors.red(x));
+      pos = 1;
+    }
+  });
+}
