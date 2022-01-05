1cce608eb2c658b9035533b31bfbc5d909fb1474
NatalyaSemenova <ambernou@gmail.com>
2022-01-05 23:57:57 +0700

    reload files

--- index.js
+++ index.js
@@ -11,18 +11,41 @@ const server = http.createServer((req, res) => {
 });
 
 const io = socket(server);
+const userList = [];
 
 io.on('connection', client => {
-    // console.log('connected', client);
+    // console.log(client);
+
+    client.on('set username', (username) => {
+        client.username = username;
+        
+        userList.push(
+            {
+                author: client.username,
+                id: client.id,
+            }
+        );
+
+        client.broadcast.emit('newUser connect', client.username);
+        client.emit('newUser connect', client.username);
+
+        client.broadcast.emit('user list', userList);
+        client.emit('user list', userList);
+    });
 
     client.on('client-msg', (data) => {
         // console.log(data);
-
         const payload = {
-            message: data.message.split('').reverse().join(''),
+            author: client.username,
+            message: data.message,
         };
+
         client.broadcast.emit('server-msg', payload);
         client.emit('server-msg', payload);
+    });
+
+    client.on('disconnect', () => {
+        client.broadcast.emit('user disconnect', client.username);
     });
 });
 
